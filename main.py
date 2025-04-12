ac7d96307007a727e0701d181eb315472a836eac
bluebird <adamboucher.01@gmail.com>
2025-04-11 22:54:59 -0400

    Added reading of text file

--- main.py
+++ main.py
@@ -54,30 +54,50 @@ def print_to_file():
         f.write("\n")
     f.close()
 
+def read_from_file():
+    file_name = input("Which file would you like to read from? No spaces and without the .txt, please: ")
+    f = open(f"{file_name}.txt", "r")
+    text = f.read()
+    f.close()
+    char_text = text.splitlines()
+    line = 0
+    while line < len(char_text):
+        print(f"Character: {char_text[line]}")
+        print(char_text[line + 1])
+        print(char_text[line + 2])
+        print(char_text[line + 3])
+        print("\n")
+        line += 4
+
 
 def user_prompt():
     while(True):
-        response = input("Hello user! Would you like to 'Create', 'Check', 'Print', or 'Exit'? ").lower()
+        response = input("Hello user! Would you like to 'Create', 'Check', 'Print', 'Read', or 'Exit'? ").lower()
         if response == "create":
             all_characters.append(create_character())
         elif response == "check":
-            print("Available characters: ")
-            for i in range(0, len(all_characters)):
-                print(f"{i}: {all_characters[i]}")
-            choice = int(input("Which character would you like to check? (Please select using index): "))
-            try:
-                character = all_characters[choice] 
-                choice = input(f"Would you like to check 'spells', check 'feats', or 'level up' for {character.name}? ").lower()
-                if choice == "spells":
-                    character.get_spells()
-                elif choice == "feats":
-                    character.get_feats()
-                elif choice == "level up":
-                    character.level_up()
-            except:
-                print("I'm sorry, something went wrong")
+            if len(all_characters) > 0:
+                print("Available characters: ")
+                for i in range(0, len(all_characters)):
+                    print(f"{i}: {all_characters[i]}")
+                choice = int(input("Which character would you like to check? (Please select using index): "))
+                try:
+                    character = all_characters[choice] 
+                    choice = input(f"Would you like to check 'spells', check 'feats', or 'level up' for {character.name}? ").lower()
+                    if choice == "spells":
+                        character.get_spells()
+                    elif choice == "feats":
+                        character.get_feats()
+                    elif choice == "level up":
+                        character.level_up()
+                except:
+                    print("I'm sorry, something went wrong")
+            else:
+                print("I'm sorry, there aren't any characters to check.")
         elif response == "print":
             print_to_file()
+        elif response == "read":
+            read_from_file()
         elif response == "exit":
             break
         else:
